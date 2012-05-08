64d67a84054ed7dce86d4623732da74b7fcd5222
crynobone <crynobone@gmail.com>
2012-05-08 13:43:52 +0800

    Add support for Hybrid\Auth to use configuration.

--- classes/auth.php
+++ classes/auth.php
@@ -1,24 +1,47 @@
 <?php namespace Hybrid;
 
-use \Auth as Laravel_Auth, \User_Role;
+use \Auth as Laravel_Auth, Config;
 
 class Auth extends Laravel_Auth
 {
+	/**
+	 * Get the current user's roles of the application.
+	 *
+	 * If the user is a guest, null should be returned.
+	 *
+	 * @return array
+	 */
 	public static function roles()
 	{
-		$user     = static::user();
-		$roles_id = array();
-		$user_id  = 0;
+		$user    = static::user();
+		$roles   = array();
+		$user_id = 0;
 
+		// only search for roles when user is logged
 		if ( ! is_null($user))
 		{
 			$user_id = $user->id;
 
-			$user_roles = User_Role::with('role')->where('user_id', '=', $user_id)->get();
-			
-			foreach ($user_roles as $role) array_push($roles_id, $role->role->name);
+			// get associated roles from configuration.
+			if ( ! (($callback = Config::get('hybrid::auth.roles')) instanceof Closure))
+			{
+				$callback = function($id, $roles)
+				{
+					// in situation config is not a closure, we will use a basic convention structure.
+					$user_roles = \User_Role::with('role')->where('user_id', '=', $user_id)->get();
+					
+					foreach ($user_roles as $role)
+					{
+						array_push($roles, $role->role->name);
+					}
+
+					return $roles;
+				}
+			}
+
+			$roles = $callback($user_id, $roles);
 		}
 
-		return $roles_id;
+		return $roles;
 	}
 }
---
+++ config/auth.php
@@ -0,0 +1,28 @@
+<?php
+
+return array(
+	/*
+	|--------------------------------------------------------------------------
+	| Retrieve Role associated to current user
+	|--------------------------------------------------------------------------
+	|
+	| Laravel Hybrid would need to know the list of roles (name) associated to 
+	| the current user, in relevant to all role defined in `\Hybrid\Acl::add_role()`.
+	|
+	| Example:
+	|
+	|   'roles' => function ($id, $roles)
+	|   {
+	|       \User_Role::with('role')->where('user_id', '=', $user_id)->get();
+	|
+	|       foreach ($user_roles as $role)
+	|       {
+	|           array_push($roles, $role->role->name);
+	|       }
+	|
+	|       return $roles;
+	|   }
+	*/
+	'roles' => null,
+
+);

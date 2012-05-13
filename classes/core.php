13b4c5f667177de664f3b9944ca871566d1ea4bc
crynobone <crynobone@gmail.com>
2012-05-13 11:57:58 +0800

    Add Image class from FuelPHP

---
+++ classes/core.php
@@ -0,0 +1,7 @@
+<?php namespace Hybrid;
+
+use Exception;
+
+class InvalidArgumentException extends Exception {}
+class OutOfBoundsException extends Exception {}
+class RuntimeException extends Exception {}

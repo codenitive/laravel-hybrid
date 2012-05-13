64db59484335270cffdb255b8d3dc7dc635026e6
crynobone <crynobone@gmail.com>
2012-05-13 12:01:15 +0800

    Add classes to namespace

--- start.php
+++ start.php
@@ -19,6 +19,16 @@ Autoloader::map(array(
 
 	'Hybrid\\Curl' => Bundle::path('hybrid').'classes/curl'.EXT,
 
+	'Hybrid\\Image'             =>  Bundle::path('hybrid').'classes/image'.EXT,
+	'Hybrid\\Image_Driver'      =>  Bundle::path('hybrid').'classes/driver'.EXT,
+	'Hybrid\\Image_Gd'          =>  Bundle::path('hybrid').'classes/gd'.EXT,
+	'Hybrid\\Image_Imagemagick' =>  Bundle::path('hybrid').'classes/imagemagick'.EXT,
+	'Hybrid\\Image_Imagick'     =>  Bundle::path('hybrid').'classes/imagick'.EXT,
+
+	'Hybrid\\InvalidArgumentException' => Bundle::path('hybrid').'classes/core'.EXT,
+	'Hybrid\\OutOfBoundsException'     => Bundle::path('hybrid').'classes/core'.EXT,
+	'Hybrid\\RuntimeException'         => Bundle::path('hybrid').'classes/core'.EXT,
+
 	'Hybrid\\Memory'          => Bundle::path('hybrid').'classes/memory'.EXT,
 	'Hybrid\\Memory_Driver'   => Bundle::path('hybrid').'classes/memory/driver'.EXT,
 	'Hybrid\\Memory_Eloquent' => Bundle::path('hybrid').'classes/memory/eloquent'.EXT,

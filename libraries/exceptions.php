222814e3517e9ca637a539263610145262911a79
crynobone <crynobone@gmail.com>
2012-05-27 00:58:26 +0800

    Add memory configuration and exceptions.php

---
+++ libraries/exceptions.php
@@ -0,0 +1,7 @@
+<?php namespace Hybrid;
+
+class Exception extends \Exception {}
+class InvalidArgumentException extends Exception {}
+class OutOfBoundsException extends Exception {}
+class RuntimeException extends Exception {}
+class AclException extends Exception { }
--- start.php
+++ start.php
@@ -12,25 +12,5 @@ Autoloader::map(array(
 	'Hybrid\\RuntimeException'         => Bundle::path('hybrid').'libraries/exceptions'.EXT,
 ));
 
-Autoloader::map(array(
-	'Hybrid\\Chart'          => Bundle::path('hybrid').'libraries/chart'.EXT,
-	'Hybrid\\Chart_Area'     => Bundle::path('hybrid').'libraries/chart/area'.EXT,
-	'Hybrid\\Chart_Bar'      => Bundle::path('hybrid').'libraries/chart/bar'.EXT,
-	'Hybrid\\Chart_Driver'   => Bundle::path('hybrid').'libraries/chart/driver'.EXT,
-	'Hybrid\\Chart_GeoMap'   => Bundle::path('hybrid').'libraries/chart/geomap'.EXT,
-	'Hybrid\\Chart_Line'     => Bundle::path('hybrid').'libraries/chart/line'.EXT,
-	'Hybrid\\Chart_Pie'      => Bundle::path('hybrid').'libraries/chart/pie'.EXT,
-	'Hybrid\\Chart_Scatter'  => Bundle::path('hybrid').'libraries/chart/scatter'.EXT,
-	'Hybrid\\Chart_Table'    => Bundle::path('hybrid').'libraries/chart/table'.EXT,
-	'Hybrid\\Chart_Timeline' => Bundle::path('hybrid').'libraries/chart/timeline'.EXT,
-	
-	'Hybrid\\Image'             =>  Bundle::path('hybrid').'libraries/image'.EXT,
-	'Hybrid\\Image_Driver'      =>  Bundle::path('hybrid').'libraries/driver'.EXT,
-	'Hybrid\\Image_Gd'          =>  Bundle::path('hybrid').'libraries/gd'.EXT,
-	'Hybrid\\Image_Imagemagick' =>  Bundle::path('hybrid').'libraries/imagemagick'.EXT,
-	'Hybrid\\Image_Imagick'     =>  Bundle::path('hybrid').'libraries/imagick'.EXT,
-
-));
-
 // Lets listen to when Hybrid bundle is started.
 Event::listen('laravel.started: hybrid', function () { Hybrid\Core::start(); });

074abadb7d7767e0b2f07a4aff8380ff06d196e5
crynobone <crynobone@gmail.com>
2012-06-14 19:33:50 +0800

    Add RESTful Response
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ libraries/response.php
@@ -0,0 +1,48 @@
+<?php namespace Hybrid;
+
+use \File, \Input, \View,
+	Laravel\Response as Laravel_Response;
+
+class Response extends Laravel_Response 
+{
+	/**
+	 * Create a new RESTful response or View response depending on requested format.
+	 * 
+	 * @param  array   $data   
+	 * @param  integer $status 
+	 * @return Response        
+	 */
+	public static function restful($data = array(), $status = 200)
+	{
+		$format     = Input::get('format');
+		$collection = Format::make($data['data']);
+
+		if (is_null($format) or ! method_exists($collection, 'to_'.$format))
+		{
+			// Response with an instanceof view, or a possible view.
+			if (isset($data['view']))
+			{
+				$view = $data['view'];
+
+				// Initiate a View instance if $view is not.
+				if ( ! ($view instanceof View)) $view = View::make($view);
+
+				$view->with($data['data']);
+
+				return static::make($view, $status);
+			}
+			else
+			{
+				// we should send an 500 server error for request that doesn't have a proper response format
+				return static::error('500');
+			}
+		}
+		else 
+		{
+			$response = $collection->{'to_'.$format}();
+			$headers  = array('Content-Type' => File::mime($format));
+
+			return Response::make($response, $status, $headers);
+		}
+	}
+}

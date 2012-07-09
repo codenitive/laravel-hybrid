3bfdcf1bce38d1a414b5d46707f54583d70edd42
Ariff Azraai <mk9206.nx@gmail.com>
2012-07-09 21:01:27 +0800

    Remove <legend> when Fieldset's name is empty

--- views/form/horizontal.php
+++ views/form/horizontal.php
@@ -4,7 +4,7 @@ foreach ($fieldsets as $fieldset) { ?>
 
 	<fieldset<?php echo HTML::attributes($fieldset->attr ?: array()); ?>>
 		
-		<legend><?php echo $fieldset->name ?: '' ?></legend>
+		<?php if( $fieldset->name ) : ?><legend><?php echo $fieldset->name ?: '' ?></legend><?php endif; ?>
 
 <?php foreach ($fieldset->controls() as $control) { ?>
 		<div class="control-group<?php echo $errors->has($control->name) ? ' error' : '' ?>">
